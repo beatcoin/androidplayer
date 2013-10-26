5d3d470242954987a256c946cf050b750bbfc344
meinhard <meinhard@benn.org>
2013-10-26 23:09:19 +0200

    add stop button, play static file

--- Beatcoin/src/main/java/org/beatcoin/player/android/MainActivity.java
+++ Beatcoin/src/main/java/org/beatcoin/player/android/MainActivity.java
@@ -1,19 +1,24 @@
 package org.beatcoin.player.android;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends Activity {
 
+    protected MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, Uri.parse(Environment.getExternalStorageDirectory().getPath() + "/Music/Sexy Boy.mp3"));
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -21,9 +26,19 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
     public void playSong(View view) {
+        mediaPlayer.start();
         Log.i("play", "clicked");
+    }
+
+    public void stopSong(View view) {
+        mediaPlayer.stop();
+        Log.i("stop", "clicked");
+    }
+
+    public void onStop() {
+        mediaPlayer.release();
     }
 
 }
